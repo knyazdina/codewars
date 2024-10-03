5358eebfa21e870bf02d9d372100b993c32bd5ec
knyaz_dina <knyaz_dina@mail.ru>
2024-10-04 00:47:04 +0300

    Rock Paper Scissors!

---
+++ 8kyu/Rock-Paper-Scissors.js
@@ -0,0 +1,10 @@
+function rps(player1, player2) {
+    if (player1 === player2) {
+        return "Draw!";
+    }
+    if ((player1 === "rock" && player2 === "scissors") || (player1 === "scissors" && player2 === "paper") || (player1 === "paper" && player2 === "rock")) {
+        return "Player 1 won!";
+    } else {
+        return "Player 2 won!";
+    }
+}
